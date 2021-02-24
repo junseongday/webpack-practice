adb826b9d0283a067c375a36e416c1dab04e8d83
junseong <junseongday@gmail.com>
2021-02-24 22:31:07 +0900

    loader

---
+++ webpack-practice/index.js
@@ -0,0 +1,9 @@
+import 'normalize.css';
+import style from './index.css';
+function component() {
+    const el = document.createElement('div');
+    el.innerHTML = 'Hello Webpack';
+    el.classList = style.helloWebpack
+    return el;
+}
+document.body.appendChild(component());
---
+++ webpack-practice/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [{
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag'
+                        }   
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    mode: 'none'
+}
